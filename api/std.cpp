26893ad7036ba6e2eaa7b1f468b1a0e76a10cc4f
ManuelWestermeier <westermeier111@gmail.com>
2024-06-02 00:09:03 +0200

    first commit

--- api/std.cpp
+++ api/std.cpp
@@ -1,8 +1,24 @@
-void run(Server *server, int port, const std::string &rootPath)
+#include <string>
+
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <cstdlib>
+#endif
+
+void run(Server *server, int port, const std::string& rootPath)
 {
-    // Construct the command to open the URL in the default web browser
-    std::string runCmd = "start http://localhost:" + std::to_string(port) + rootPath;
+    // Construct the URL
+    std::string url = "http://localhost:" + std::to_string(port) + rootPath;
+
+#ifdef _WIN32
+    // Use ShellExecute to open the URL in the default browser without showing a console window on Windows
+    ShellExecute(0, 0, url.c_str(), 0, 0, SW_SHOWDEFAULT);
+#else
+    // For other systems, use system call (this will still show a console window)
+    std::string runCmd = "xdg-open " + url;  // For Linux
     system(runCmd.c_str());
+#endif
 
     // Start the server and listen on the specified port
     if (!server->listen("0.0.0.0", port))
